30e0e737da00a0a32ee8dc7e525bf6912040019a
vprahaladhan <vprahaladhan@users.noreply.github.com>
2021-03-05 13:44:08 +1100

    slow appearing page sections.

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.block-intro');
+sr.reveal('.skills');
+sr.reveal('.website');
+sr.reveal('.work-experience');
+sr.reveal('.education');
+sr.reveal('.project-container');
+sr.reveal('.contact-container');
